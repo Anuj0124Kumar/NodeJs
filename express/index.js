cf6e3c350fd9a5904250afc8ddd8c4512466955c
Anuj0124Kumar <anuj0214@gmail.com>
2023-03-02 18:23:17 +0530

    partial

--- express/index.js
+++ express/index.js
@@ -2,20 +2,27 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+// const pathdir=path.join(__dirname,'public');
+// app.use(express.static(pathdir))
 
-const pathdir=path.join(__dirname,'public');
-app.use(express.static(pathdir))
+app.set("view engine", "ejs");
 
-app.get("/",(req,res)=>{
-    res.sendFile(`${pathdir}/inde.html`);
-})
-app.get("/about",(req,res)=>{
-    res.sendFile(`${pathdir}/home.html`);
-})
-app.get("/download",(req,res)=>{
-    res.download(`${pathdir}/inde.html`)
-})
+console.log(app.get("view engine"));
+console.log(path.join(__dirname), "views");
 
-app.listen(3000,()=>{
-    console.log("server established");
-})
+app.get("/", (req, res) => {
+  // res.sendFile(`${pathdir}/inde.html`);
+  res.render("inde",{
+   title:"index"
+  });
+});
+app.get("/about", (req, res) => {
+  // res.sendFile(`${pathdir}/home.html`);
+});
+app.get("/download", (req, res) => {
+  // res.download(`${pathdir}/inde.html`)
+});
+
+app.listen(3000, () => {
+  console.log("server established");
+});
